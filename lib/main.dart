c3f9ee764f7a49bf812599e48930b281ac631e7c
Yao Josué <jyao2000@yahoo.fr>
2020-05-18 16:22:31 +0200

    first scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello word'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+              image: NetworkImage('https://www.mining.com/wp-content/uploads/2018/08/unnamed.jpg')
+          ),
+        ),
       ),
     ),
   );
